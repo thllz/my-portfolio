c5673446e6e69ceba3b41e7e4bb785742a592b21
thllz <thelisommagnos@hotmail.com>
2022-11-26 00:37:16 -0300

    feat: hamburguer menu and some fixes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
-import Header from './components/Header';
+import { scaleDown as Menu } from 'react-burger-menu'
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <div >
+      <>
+        <Menu className='menu'>
+          <a id="home" className="menu-item" href="/">Inicio</a>
+          <a id="about" className="menu-item" href="/about">Sobre mim</a>
+          <a id="projects" className="menu-item" href="/contact">Projetos</a>
+          <a id="contact" className="menu-item" href="/contact">Contatos</a>
+        </Menu>
+      </>
+      <div className='header'>
+        <h1> Olá, bem vindo(a) ao meu portfólio!</h1>
+      </div>
+      <div>
+        <p>Sobre mim</p>
+      </div>
+      <div>
+        <p>Projetos</p>
+      </div>
+      <div>
+        <p>Contatos</p>
+      </div>
     </div>
   );
 }
