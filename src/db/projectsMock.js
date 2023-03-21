f17746f7e9f7e5d3f9efd89cf959da19d3abc703
thllz <thelisommagnos@hotmail.com>
2023-03-21 20:38:01 -0300

    fix: layout from /contacts route

---
+++ src/db/projectsMock.js
@@ -0,0 +1,184 @@
+export const projectMock = [
+  {
+    name: 'Lessons Learned',
+    desc: 'Projeto desenvolvido utilizando HTML e CSS puro, seguindo a semântica correta para os elementos.',
+    tags: ['html', 'css', 'html semantic'],
+    inGroup: 0,
+    class: 'fundamentos'
+  },
+  {
+    name: 'Playground Functions',
+    desc: 'Projeto com o foco em desenvolver funções mais complexas utilizando JavaScript.',
+    tags: ['javaScript'],
+    inGroup: 0,
+    class: 'fundamentos'
+  },
+  {
+    name: 'Arte com Pixels',
+    desc: 'Projeto feito como o objetivo de ser um editor de arte com pixels, sendo possível que o usuário escolha a cor na paleta de cores e pintar o que quiser. ',
+    tags: ['html', 'javaScript', 'css'],
+    inGroup: 0,
+    class: 'fundamentos'
+  },
+  {
+    name: 'TrybeWarts',
+    desc: 'Projeto proposto com a ideia de desenvolver um formulário para a inscrição em uma escola inspirada na série de filmes Harry Potter de J.K. Rowling.',
+    tags: ['html', 'javaScript', 'css'],
+    inGroup: 0,
+    class: 'fundamentos'
+  },
+  {
+    name: 'Testes Unitários',
+    desc: 'Projeto apresentado com a ideia de desenvolver funções em JavaScript com cobertura total de testes utilizando o framework de testes Jest.',
+    tags: ['javaScript', 'jest', 'es6'],
+    inGroup: 0,
+    class: 'fundamentos'
+  },
+  {
+    name: 'Zoo Functions',
+    desc: 'Projeto com ênfase no uso das High Order Functions (HOFs), foram desenvolvidas funções em JavaScript ES6 que fossem capazes de buscar informações sendo manipuladas com uso das HOFs.',
+    tags: ['javaScript', 'hofs', 'es6'],
+    inGroup: 0,
+    class: 'fundamentos'
+  },
+  {
+    name: 'Shopping Cart',
+    desc: 'Projeto desenvolvido com a ideia de criar um carrinho de compras em uma aplicação de loja online feita com o consumo de uma API de produtos, que tivesse a capacidade de somar o valor dos itens, salva-los caso a pagina fosse atualizado, entre outras features',
+    tags: ['javaScript', 'tdd', 'API', 'fetch'],
+    inGroup: 0,
+    class: 'frontend'
+  },
+  {
+    name: 'Solar System',
+    desc: 'Projeto desenvolvido com o uso do React em Classe com o objetivo de renderizar planetas do sistema solar de forma componentizada.',
+    tags: ['javaScript', 'html', 'css', 'react.JS'],
+    inGroup: 0,
+    class: 'frontend'
+  },
+  {
+    name: 'Tryunfo',
+    desc: 'Projeto feito com o objetivo de recriar um jogo no estilo Super Trunfo utilizando React.JS.',
+    tags: ['javaScript', 'html', 'css', 'react.JS'],
+    inGroup: 0,
+    class: 'frontend'
+  },
+  {
+    name: 'TrybeTunes',
+    desc: 'Projeto proposto com a ideia de criar uma aplicação que funcionasse como um player de músicas online utilizando React.JS e consumindo a API do Spotify.',
+    tags: ['javaScript', 'html', 'css', 'react.JS', 'API', 'fetch'],
+    inGroup: 0,
+    class: 'frontend'
+  },
+  {
+    name: 'Front-end Online Store',
+    desc: 'Projeto em grupo proposto com o objetivo de desenvolver uma aplicação similar a uma loja online, na qual foi feita utilizando o TDD (Test-Driven Development) com o consumo de uma API de produtos do Mercado Livre.',
+    tags: ['javaScript', 'tdd', 'API', 'fetch', 'group', 'metologia agil', 'kanban', 'scrum'],
+    inGroup: 1,
+    class: 'frontend'
+  },
+  {
+    name: 'React Testing Library',
+    desc: 'Projeto feito com o objetivo de praticar o uso da biblioteca de testes Jest e a RTL (React Testing library) do React. Testes automatizados.',
+    tags: ['javaScript', 'testes', 'rtl', 'jest', 'testes automatizados'],
+    inGroup: 0,
+    class: 'frontend'
+  },
+  {
+    name: 'TrybeWallet',
+    desc: 'Projeto desenvolvido com a proposta de criar uma carteira de controles de gasto com conversores de moedas.',
+    tags: ['html', 'css', 'bootstrap', 'javaScript'],
+    inGroup: 0,
+    class: 'frontend'
+  },
+  {
+    name: 'Trivia',
+    desc: 'Projeto em grupo proposto com a ideia de desenvolvermos um jogo de Trivia com tela de login, validação de acertos e erros, com consumo de uma API de perguntas e respostas.',
+    tags: ['javaScript', 'tdd', 'API', 'fetch', 'group', 'metologia agil', 'kanban', 'scrum', 'game', 'trivia'],
+    inGroup: 1,
+    class: 'frontend'
+  },
+  {
+    name: 'Star Wars Planets Search',
+    desc: 'Projeto desenvolvido com o objetivo de criarmos uma aplicação em que fosse possível realizar diversos filtros com os dados de planetas do Star Wars fornecidas através de uma API.',
+    tags: ['html', 'javaScript', 'css', 'API', 'fetch', 'hofs'],
+    inGroup: 0,
+    class: 'frontend'
+  },
+  {
+    name: 'App de Receitas',
+    desc: 'Projeto em grupo mais complexo que consistia em desenvolver uma aplicação responsiva em que fosse possível pesquisar por receitas de comidas e bebidas, favoritá-las, compartilhar nas redes sociais, salvá-las em seu acesso também com validação por conta do Google.',
+    tags: ['javaScript', 'react.JS', 'css', 'bootstrap', 'context API', 'API', 'group'],
+    inGroup: 1,
+    class: 'frontend'
+  },
+  {
+    name: 'Docker todo list',
+    desc: 'Projeto proposto para treinarmos o uso do Docker. Criando containers. Criando conexões entre os containers. Orquestrar seu funcionamento através do docker compose.',
+    tags: ['docker', 'container', 'compose'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  {
+    name: 'All for One',
+    desc: 'Projeto com o objetivo de treinarmos a criação de QUERYs MySql.',
+    tags: ['mysql', 'query', 'docker', 'compose', 'container'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  {
+    name: 'One for All',
+    desc: 'Projeto desenvolvido usando a técnica de normalização de tabelas, também fazendo criações de QUERYs mais complexas.',
+    tags: ['mysql', 'normalização', 'docker', 'query'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  {
+    name: 'Talker Manager',
+    desc: 'Projeto proposto a desenvolvermos uma API REST, utilizando NODE.JS, EXPRESS com MySql',
+    tags: ['NODE.JS', 'mysql', 'EXPRESS', 'API', 'RESTFUL'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  {
+    name: 'Store Manager',
+    desc: 'Projeto com o objetivo de criarmos uma API REST, seguindo as normas da arquitetura de software em camadas (MSC), com uso do NODE.JS, EXPRESS, MySql e com cobertura total de testes.',
+    tags: ['EXPRESS', 'NODE.JS', 'EXPRESS', 'API', 'RESTFUL', 'test', 'jest', 'mocha', 'sinon', 'stub', 'msc'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  {
+    name: 'Blogs API',
+    desc: 'Projeto feito com a ideia de criarmos uma API de blogs, com CRUD completo, validações, uso de ORM (Sequelize).',
+    tags: ['EXPRESS', 'NODE.JS', 'EXPRESS', 'API', 'RESTFUL', 'blog', 'orm', 'sequelize', 'CRUD'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  {
+    name: 'Trybesmith',
+    desc: 'Projeto curto com a proposta de criarmos uma API REST, com CRUD completo, seguindo as normas da arquitetura de software em camadas (MSC), utilizando o TypeScript',
+    tags: ['typeScript','EXPRESS', 'NODE.JS', 'EXPRESS', 'API', 'RESTFUL', 'CRUD'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  {
+    name: 'Trybers and Dragons',
+    desc: 'Projeto desenvolvido com a proposta de aplicarmos os princípios da arquitetura SOLID e os princípios de POO.',
+    tags: ['typeScript', 'NODE.JS', 'POO', 'SOLID', 'API', 'RESTFUL'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  {
+    name: 'Trybe Futebol Clube',
+    desc: 'Projeto que se trata de um site informativo sobre partidas e classificações de futebol! Projeto conteinerizado com docker. API desenvolvida seguindo o método TDD. Página de login autenticada com validações, JWT e BCRYPT para criptografia de senhas.',
+    tags: ['typescript', 'NODE.JS', 'API', 'RESTFUL', 'TDD', 'DOCKER', 'COMPOSE'],
+    inGroup: 0,
+    class: 'backend'
+  },
+  // {
+  //   name: '',
+  //   desc: '',
+  //   tags: [],
+  //   inGroup: 0,
+  //   class: ''
+  // },
+]
--- src/pages/Contacts/Contacts.js
+++ src/pages/Contacts/Contacts.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
-import { SiLinkedin, SiGithub, SiWhatsapp } from 'react-icons/si'
+import { SiLinkedin, SiGithub, SiWhatsapp, SiCoffeescript } from 'react-icons/si'
 import { HiOutlineMail } from 'react-icons/hi'
 import './Contacts.css'
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function Contacts() {
+  const [state, handleSubmit] = useForm("xoqzndyy");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+
   return (
     <>
       <Header />
@@ -16,11 +22,59 @@ export default function Contacts() {
             <h4 className='emailText'>thelisommagnos@hotmail.com</h4>
           </div>
           <div className='wppBox'>
-            <SiWhatsapp className='wppIcon'/>
+            <SiWhatsapp className='wppIcon' />
             <h4 className='wppNumber'>(31)98260-3250</h4>
           </div>
-          {/* <SiLinkedin className='linkedinTag' /> */ }
-          {/* <SiGithub className='githubTag' /> */ }
+        </div>
+        <div className='socialLinks'>
+          <div className='linkedinBox'>
+            <a className='linkedinBtnContacts' href='https://www.linkedin.com/in/thelisom/' target='_tab'>
+              <SiLinkedin size={ 50 } />
+            </a>
+          </div>
+          <div className='githubBox'>
+            <a className='githubBtnContacts' href='https://github.com/thllz' target='_tab'>
+              <SiGithub size={ 50 } />
+            </a>
+          </div>
+        </div>
+        <div className='mainFormContact'>
+          <div className='formTextHeader'>
+          <SiCoffeescript size={30}/>
+          <h2>Pegue um café e bora bater um papo!</h2>
+          </div>
+          <form className='formContact' onSubmit={ handleSubmit }>
+            <label htmlFor="email">
+              Email:
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder='email@email.com'
+            />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={ state.errors }
+            />
+            <label>
+              Mensagem:
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              placeholder='Olá, tudo bem?...'
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={ state.errors }
+            />
+            <button type="submit" disabled={ state.submitting }>
+              Enviar
+            </button>
+          </form>
         </div>
       </div>
     </>
